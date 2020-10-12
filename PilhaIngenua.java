c4b08a9a07b602bca15d04f8ad27997b794bb2f7
Daniel Yuji Yamada <72767423+daniyuji@users.noreply.github.com>
2020-10-12 18:34:52 -0300

    Add files via upload

---
+++ PilhaIngenua.java
@@ -0,0 +1,22 @@
+package Pilhas;
+
+public class PilhaIngenua {
+	protected final int MAX = 1000000;
+	protected Integer[] pilha;
+	
+	PilhaIngenua(){pilha = new Integer [MAX]; }
+	
+	void add(int newElement) {
+		int i;
+		for(i = 0; pilha[i] != null; i++);
+		pilha[i] = newElement;
+	}
+	
+	int remove() {
+		int i;
+		for(i = 0; pilha[i] != null; i++);
+		int tmp = pilha[i - 1];
+		pilha[i - 1] = null;
+		return tmp;
+	}
+}
